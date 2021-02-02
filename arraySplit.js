8072fed4600c6490e588cbbcf61c6a2ba0ac426c
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-02 17:08:50 +0600

    add some js advanced about intervew questions

---
+++ arraySplit.js
@@ -0,0 +1,11 @@
+const rolls = [1,2,3,4,5,6,7,8,9];
+const slice = rolls.slice(1, 6);
+console.log(slice);
+console.log(rolls);
+
+const splice = rolls.splice(1, 4);
+console.log(splice);
+console.log(rolls);
+
+const join = rolls.join('-');
+console.log(join);
